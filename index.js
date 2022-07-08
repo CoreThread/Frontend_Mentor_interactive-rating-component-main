ce96239d2b2b83d43e1aa45d88003a78f1fc3b32
CoreThread <52364876+CoreThread@users.noreply.github.com>
2022-07-08 11:34:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let n, x;
+function ratingNumber(element)
+{
+    let n = element.value;
+    document.getElementById('final').innerText = n;
+    document.getElementsByClassName('xxx').style
+}
+function pageTwo()
+{
+    document.getElementsByClassName('mainBox')[0].style.display="none";
+    document.getElementsByClassName('outBox')[0].style.display="flex";
+}
